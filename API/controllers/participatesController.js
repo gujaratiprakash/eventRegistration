0e08c8e9b6fe457eb19b65d36bc82c04c4b34e22
PrakashGujarati <prakashgujarati.work@gmail.com>
2023-07-15 22:47:45 +0530

    update api participantes

--- API/controllers/participatesController.js
+++ API/controllers/participatesController.js
@@ -1,9 +1,29 @@
 const mongoose = require('mongoose');
 
+// exports.createParticipates = async (req, res) => {
+//     try {
+//         const participatesCollectionSchema = new mongoose.Schema({}, { strict: false })
+//         const ParticipatesCollection = mongoose.model('participates', participatesCollectionSchema)
+//         const ParticipatesCollectionObj = new ParticipatesCollection(req.body);
+//         await ParticipatesCollectionObj.save();
+//         res.status(201).json({
+//             "msg": "Data Saved Successfully"
+//         });
+//     } catch (err) {
+//         res.status(400).json({ error: err.message });
+//     }
+// };
+let ParticipatesCollection;
+
+try {
+  ParticipatesCollection = mongoose.model('participates');
+} catch {
+  const participatesCollectionSchema = new mongoose.Schema({}, { strict: false });
+  ParticipatesCollection = mongoose.model('participates', participatesCollectionSchema);
+}
+
 exports.createParticipates = async (req, res) => {
     try {
-        const participatesCollectionSchema = new mongoose.Schema({}, { strict: false })
-        const ParticipatesCollection = mongoose.model('participates', participatesCollectionSchema)
         const ParticipatesCollectionObj = new ParticipatesCollection(req.body);
         await ParticipatesCollectionObj.save();
         res.status(201).json({
